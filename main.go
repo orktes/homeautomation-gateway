7d5c72c72c6fcb9c22f6cdd89c14ef031c3fb5a2
Jaakko Lukkari <jaakko@unity3d.com>
2018-01-16 00:07:36 +0200

    Dump traffic

--- main.go
+++ main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"io"
 	"net/http"
+	"net/http/httputil"
 	"os"
 )
 
 func main() {
 	http.ListenAndServe(":"+os.Getenv("PORT"), http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		d, _ := httputil.DumpRequest(r, true)
+		println(d)
 		io.WriteString(w, "TO BE DONE")
 	}))
 }
