6748eb6fde7fba0df457c4f66d0d7ae6d69809ff
Jaakko Lukkari <jaakko@unity3d.com>
2018-01-16 00:30:25 +0200

    Quick hack

--- main.go
+++ main.go
@@ -1,16 +1,64 @@
 package main
 
 import (
-	"io"
+	"bytes"
 	"net/http"
-	"net/http/httputil"
+	"net/url"
 	"os"
+
+	"text/template"
+
+	"github.com/gin-gonic/gin"
 )
 
+var signupTemplate = template.Must(template.New("signup").Parse(`
+	<!DOCTYPE html>
+	<html>
+		<head>
+		<meta charset="UTF-8">
+		<title>Homeautomation auth</title>
+		</head>
+		<body>
+		Copy homeautomation client id found from your logs here<br />
+		<form action="{{.redirect_uri}}" method="get">
+			{{ range $key, $values := .params }}
+				{{ range $value := $values }}
+					<input type="hidden" name="{{$key}}" value="{{$value}}" />
+				{{ end }}
+			{{ end }}
+			<input type="text" name="code" value="" />
+			
+			<input type="submit" value="OK!"/>
+		</form>
+		</body>
+	</html>
+	`))
+
+func oauthAuthorize(c *gin.Context) {
+	redirect := c.Query("redirect_uri")
+	parsedRedirect, _ := url.Parse(redirect)
+
+	b := &bytes.Buffer{}
+	signupTemplate.Execute(b, map[string]interface{}{"redirect_uri": redirect, "params": parsedRedirect.Query()})
+
+	c.Data(http.StatusOK, "text/html; charset=utf-8", b.Bytes())
+}
+
+func oauthAccessToken(c *gin.Context) {
+	code := c.Query("code")
+
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"access_token": code,
+		"token_type":   "bearer",
+	})
+}
+
 func main() {
-	http.ListenAndServe(":"+os.Getenv("PORT"), http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		d, _ := httputil.DumpRequest(r, true)
-		println(d)
-		io.WriteString(w, "TO BE DONE")
-	}))
+	r := gin.Default()
+	oauth := r.Group("/oauth")
+	{
+		oauth.GET("/authorize", oauthAuthorize)
+		oauth.POST("/access_token", oauthAccessToken)
+	}
+	r.Run(":" + os.Getenv("PORT"))
 }
